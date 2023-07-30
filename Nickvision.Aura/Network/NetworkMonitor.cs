1660d53df32c4d7c3e026e46ee9b5c64587e7a54
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-07-30 19:24:35 +0300

    Rework NetworkMonitor

--- Nickvision.Aura/Network/NetworkMonitor.cs
+++ Nickvision.Aura/Network/NetworkMonitor.cs
@@ -16,24 +16,43 @@ public class NetworkMonitor : IDisposable
     private INetworkManager? _networkManager;
     private string[]? _networkAddresses;
     
-    private event Action<NetworkState>? _stateChanged;
+    private event EventHandler<NetworkState>? _stateChanged;
 
     /// <summary>
     /// Construct NetworkMonitor
     /// </summary>
-    public NetworkMonitor()
+    private NetworkMonitor()
     {
         _disposed = false;
+    }
+
+    /// <summary>
+    /// Create and setup new network monitor
+    /// </summary>
+    /// <returns>Network monitor</returns>
+    public static async Task<NetworkMonitor> NewAsync()
+    {
+        var netmon = new NetworkMonitor();
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
         {
-            #pragma warning disable CS4014
-            SetupNetworkManager();
-            #pragma warning restore CS4014
+            try
+            {
+                netmon._dbusConnection = new Connection(Address.System);
+                await netmon._dbusConnection.ConnectAsync();
+                netmon._networkManager = netmon._dbusConnection.CreateProxy<INetworkManager>("org.freedesktop.NetworkManager", new ObjectPath("/org/freedesktop/NetworkManager"));
+                await netmon._networkManager.WatchStateChangedAsync(state => netmon._stateChanged?.Invoke(netmon, state));
+            }
+            catch
+            {
+                netmon._networkManager = null;
+                netmon.SetupPing();
+            }
         }
         else
         {
-            SetupPing();
+            netmon.SetupPing();
         }
+        return netmon;
     }
 
     /// <summary>
@@ -44,12 +63,12 @@ public class NetworkMonitor : IDisposable
     /// <summary>
     /// Occurs when network state is changed
     /// </summary>
-    public event Action<NetworkState> StateChanged
+    public event EventHandler<NetworkState> StateChanged
     {
         add
         {
             _stateChanged += value;
-            Task.Run(async () => _stateChanged.Invoke(await GetStateAsync()));
+            Task.Run(async () => _stateChanged.Invoke(this, await GetStateAsync()));
         }
 
         remove
@@ -81,25 +100,6 @@ public class NetworkMonitor : IDisposable
     }
     
     /// <summary>
-    /// Setup network monitor to use NetworkManager
-    /// </summary>
-    private async Task SetupNetworkManager()
-    {
-        try
-        {
-            _dbusConnection = new Connection(Address.System);
-            await _dbusConnection.ConnectAsync();
-            _networkManager = _dbusConnection.CreateProxy<INetworkManager>("org.freedesktop.NetworkManager", new ObjectPath("/org/freedesktop/NetworkManager"));
-            await _networkManager.WatchStateChangedAsync(change => _stateChanged?.Invoke(change));
-        }
-        catch
-        {
-            _networkManager = null;
-            SetupPing();
-        }
-    }
-    
-    /// <summary>
     /// Setup network monitor to use dotnet NetworkChange and ping
     /// </summary>
     private void SetupPing()
@@ -107,7 +107,7 @@ public class NetworkMonitor : IDisposable
         _networkAddresses = new []{ "8.8.8.8", "http://www.baidu.com", "http://www.aparat.com" };
         NetworkChange.NetworkAvailabilityChanged += async (sender, e) =>
         {
-            _stateChanged?.Invoke(await PingReliableSitesAsync());
+            _stateChanged?.Invoke(this, await PingReliableSitesAsync());
         };
     }
     
