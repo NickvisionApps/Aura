066e7632273324668000e0b2be3fcb4d5d34531f
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-09-05 22:45:23 +0300

    Add LibSecret support

--- Nickvision.Aura/Keyring/SystemCredentialManager.cs
+++ Nickvision.Aura/Keyring/SystemCredentialManager.cs
@@ -8,10 +8,25 @@ namespace Nickvision.Aura.Keyring;
 /// Object to access system credential manager
 /// </summary>
 /// <remarks>Uses Windows Credential Manager on Windows and LibSecret on Linux</remarks>
-public static class SystemCredentialManager
+public static partial class SystemCredentialManager
 {
+    [LibraryImport("libsecret-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
+    private static partial nint secret_schema_new(string name, int flags, nint args);
+    [LibraryImport("libsecret-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
+    private static partial string secret_password_lookup_sync(nint schema, nint cancellable, nint error, nint args);
+    [LibraryImport("libsecret-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
+    [return:MarshalAs(UnmanagedType.I1)]
+    private static partial bool secret_password_store_sync(nint schema, nint collection, string label, string password, nint cancellable, nint error, nint args);
+    [LibraryImport("libsecret-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
+    [return:MarshalAs(UnmanagedType.I1)]
+    private static partial bool secret_password_clear_sync(nint schema, nint cancellable, nint error, nint args);
+    [LibraryImport("libsecret-1.so.0")]
+    private static partial void secret_schema_unref(nint schema);
+
+    private const int SECRET_SCHEMA_NONE = 0;
+
     /// <summary>
-    /// Get password from credential manager
+    /// Get keyring's password from credential manager
     /// </summary>
     /// <param name="name">Keyring name</param>
     /// <returns>Keyring password or null</returns>
@@ -21,7 +36,13 @@ public static class SystemCredentialManager
         {
             return CredentialManager.ReadCredential(name)?.Password ?? null;
         }
-        // TODO: Linux
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            var schema = secret_schema_new(name, SECRET_SCHEMA_NONE, IntPtr.Zero);
+            var password = secret_password_lookup_sync(schema, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            secret_schema_unref(schema);
+            return password;
+        }
         throw new PlatformNotSupportedException();
     }
 
@@ -38,7 +59,38 @@ public static class SystemCredentialManager
             CredentialManager.WriteCredential(name, "NickvisionKeyring", password, CredentialPersistence.LocalMachine);
             return password;
         }
-        // TODO: Linux
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            var schema = secret_schema_new(name, SECRET_SCHEMA_NONE, IntPtr.Zero);
+            var success = secret_password_store_sync(schema, IntPtr.Zero, name, password, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            secret_schema_unref(schema);
+            if (!success)
+            {
+                throw new Exception();
+            }
+            return password;
+        }
+        throw new PlatformNotSupportedException();
+    }
+
+    /// <summary>
+    /// Delete keyring's password from credential manager
+    /// </summary>
+    /// <param name="name">Keyring name</param>
+    public static void DeletePassword(string name)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            CredentialManager.DeleteCredential(name);
+            return;
+        }
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            var schema = secret_schema_new(name, SECRET_SCHEMA_NONE, IntPtr.Zero);
+            secret_password_clear_sync(schema, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            secret_schema_unref(schema);
+            return;
+        }
         throw new PlatformNotSupportedException();
     }
 }
