019727187ec7b557d9685edf32f6cdff4d309af3
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-09-06 23:47:03 +0300

    Fixes in PasswordGenerator

--- Nickvision.Aura/Keyring/PasswordGenerator.cs
+++ Nickvision.Aura/Keyring/PasswordGenerator.cs
@@ -23,54 +23,59 @@ public class PasswordGenerator
     private PasswordContent _contentFlags;
     
     /// <summary>
-    /// Construct password generator
+    /// Constructs password generator
     /// </summary>
     /// <param name="contentFlags">Flags to determine possible characters in password</param>
     public PasswordGenerator(PasswordContent contentFlags = PasswordContent.Numeric | PasswordContent.Uppercase | PasswordContent.Lowercase | PasswordContent.Special)
     {
         _chars = new();
-        SetContentFlags(contentFlags);
+        ContentFlags = contentFlags;
     }
     
     /// <summary>
-    /// Set possible characters for already existing PasswordGenerator
+    /// Possible characters for PasswordGenerator
     /// </summary>
-    /// <param name="contentFlags">Flags to determine possible characters in password</param>
-    public void SetContentFlags(PasswordContent contentFlags)
+    public PasswordContent ContentFlags
     {
-        _contentFlags = contentFlags;
-        if (_contentFlags.HasFlag(PasswordContent.Numeric))
+        get => _contentFlags;
+        
+        set
         {
-            _chars.AddRange(new []{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
-        }
-        if (_contentFlags.HasFlag(PasswordContent.Uppercase))
-        {
-            _chars.AddRange(new []
+            _contentFlags = value;
+            _chars.Clear();
+            if (_contentFlags.HasFlag(PasswordContent.Numeric))
             {
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-            });
-        }
-        if (_contentFlags.HasFlag(PasswordContent.Lowercase))
-        {
-            _chars.AddRange(new []
+                _chars.AddRange(new []{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+            }
+            if (_contentFlags.HasFlag(PasswordContent.Uppercase))
             {
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-            });
-        }
-        if (_contentFlags.HasFlag(PasswordContent.Special))
-        {
-            _chars.AddRange(new []
+                _chars.AddRange(new []
+                {
+                    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+                });
+            }
+            if (_contentFlags.HasFlag(PasswordContent.Lowercase))
             {
-                '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':',
-                ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'
-            });
+                _chars.AddRange(new []
+                {
+                    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+                });
+            }
+            if (_contentFlags.HasFlag(PasswordContent.Special))
+            {
+                _chars.AddRange(new []
+                {
+                    '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':',
+                        ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'
+                });
+            }
         }
     }
     
     /// <summary>
-    /// Generate new password
+    /// Generates new password
     /// </summary>
     /// <param name="length">Password length</param>
     /// <returns>A new random password</returns>
