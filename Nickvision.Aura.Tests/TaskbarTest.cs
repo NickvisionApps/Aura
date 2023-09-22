c41ef9ca4ebd90e2bfb86f4e1adeb872c82c4423
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-09-22 06:54:54 +0300

    Skip taskbar test in github actions

--- Nickvision.Aura.Tests/TaskbarTest.cs
+++ Nickvision.Aura.Tests/TaskbarTest.cs
@@ -1,14 +1,16 @@
+using System;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Nickvision.Aura.Tests;
 
-public class TaskbarItemTest
+public class TaskbarTest
 {
     [SkippableFact]
-    public async Task ConnectTest()
+    public async Task ItemConnectTest()
     {
         Skip.IfNot(RuntimeInformation.IsOSPlatform(OSPlatform.Linux));
+        Skip.IfNot(string.IsNullOrEmpty(Environment.GetEnvironmentVariable("GITHUB_ACTIONS")));
         using var t = await Taskbar.TaskbarItem.ConnectAsync("test.desktop");
         Assert.NotNull(t);
     }
