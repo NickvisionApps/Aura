8e398f2faff2d01985a16493e00af64c35bedeb1
Nick Logozzo <nlogozzo225@gmail.com>
2023-09-26 18:02:18 -0400

    Create UpdaterTest.cs

---
+++ Nickvision.Aura.Tests/UpdaterTest.cs
@@ -0,0 +1,20 @@
+﻿using Nickvision.Aura.Update;
+using System;
+using System.Threading.Tasks;
+
+namespace Nickvision.Aura.Tests;
+
+public class UpdaterTest
+{
+    [Fact]
+    public async Task LatestUpdateTest()
+    {
+        var currentVersion = new Version("2023.8.0");
+        Aura.Init("org.nickvision.test", "Nickvision Test");
+        Aura.Active.AppInfo.SourceRepo = new Uri("https://github.com/NickvisionApps/Denaro");
+        var updater = new Updater();
+        var latestUpdate = await updater.GetCurrentStableVersionAsync();
+        Assert.NotNull(latestUpdate);
+        Assert.True(latestUpdate > currentVersion);
+    }
+}
