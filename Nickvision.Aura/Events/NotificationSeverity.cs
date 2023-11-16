d8677a2cb1137cdf34942fa7dc7180d71f100576
Nick Logozzo <nlogozzo225@gmail.com>
2023-11-16 16:08:02 -0500

    Events - Add

---
+++ Nickvision.Aura/Events/NotificationSeverity.cs
@@ -0,0 +1,12 @@
+﻿namespace Nickvision.Aura.Events;
+
+/// <summary>
+/// Severities for a notification
+/// </summary>
+public enum NotificationSeverity
+{
+    Informational = 0,
+    Success,
+    Warning,
+    Error
+}
